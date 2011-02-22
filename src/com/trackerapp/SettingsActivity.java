144c8ddc59a115d46983d2d6dccec3c97db51bd6
Admini <admini@admini-eME642G.(none)>
2011-02-22 14:14:52 +0200

    raw seekbar added

--- src/com/trackerapp/SettingsActivity.java
+++ src/com/trackerapp/SettingsActivity.java
@@ -2,11 +2,42 @@ package com.trackerapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class SettingsActivity extends Activity {
-
+public class SettingsActivity extends Activity implements OnSeekBarChangeListener {
+	SeekBar seekFreq;
+	TextView txtTest;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+        
+        seekFreq = (SeekBar) findViewById(R.id.seekFreq);
+        txtTest = (TextView) findViewById(R.id.txtTest);
+        
+        seekFreq.setOnSeekBarChangeListener(this);
     }
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		txtTest.setText(progress);
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+  
 }
